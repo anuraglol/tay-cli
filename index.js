22d98a5cbbe7f0aba3ede16c83d57728486381ae
Anurag <kr.anurag24@gmail.com>
2022-01-29 05:47:12 +0000

    ✨feat: add some commands

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 #!usr/bin/env node
 
-import chalk from "chalk"
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
 
-console.log(chalk.bgGreen("yoo"))
+console.log(chalk.bgGreen("yoo"));
+
+let playername;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+const welcome = async () => {
+  const title = chalkAnimation.rainbow("Yoo! I hope ya brought pizza");
+
+  await sleep();
+  title.stop();
+
+  console.log(`
+  Yoo! \n
+  ${chalk.bgGreen("are ya kiding me")}
+  `);
+};
+
+await welcome();
+
+const askName = async () => {
+  const answer = await inquirer.prompt({
+    name: "player_name",
+    type: "input",
+    message: "yoo! what's ur name lil boi",
+
+    default() {
+      return "Player";
+    },
+  });
+
+  playername = answer.player_name
+};
+
+
+await askName()
