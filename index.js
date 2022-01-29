67ec3d0b857ef630bc376816e78cdc17094f3988
Anurag <kr.anurag24@gmail.com>
2022-01-29 05:56:49 +0000

    ✨feat: add a mcq ques

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 import inquirer from "inquirer";
+import { createSpinner } from "nanospinner"
 
 console.log(chalk.bgGreen("yoo"));
 
-let playername;
+let playerName;
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
@@ -24,6 +25,18 @@ const welcome = async () => {
 
 await welcome();
 
+async function handleAnswer(isCorrect) {
+  const spinner = createSpinner("Checking answer...").start();
+  await sleep();
+
+  if (isCorrect) {
+    spinner.success({ text: `Nice work ${playerName}. That's a legit answer` });
+  } else {
+    spinner.error({ text: `💀💀💀 Game over, you lose ${playerName}!` });
+    process.exit(1);
+  }
+}
+
 const askName = async () => {
   const answer = await inquirer.prompt({
     name: "player_name",
@@ -35,8 +48,20 @@ const askName = async () => {
     },
   });
 
-  playername = answer.player_name
+  playerName = answer.player_name;
 };
 
+await askName();
 
-await askName()
+const question1 = async () => {
+  const answers = await inquirer.prompt({
+    name: "question_1",
+    type: "list",
+    message: "yoo! wots up \n",
+    choices: ["im fine, wbu", "sutup", "no u"],
+  });
+
+  return handleAnswer(answers.question_1 == "no u")
+};
+
+await question1()
